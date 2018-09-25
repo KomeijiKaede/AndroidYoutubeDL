b0240fdb48e57177e2b53338592d10cf8b3a17ff
KomeijiKaede <KomeijiKoishi200058@gmail.com>
2018-09-25 15:02:59 +0900

    .

---
+++ app/src/main/java/net/teamfruit/androidyoutubedl/MainActivity.kt
@@ -0,0 +1,34 @@
+package net.teamfruit.androidyoutubedl
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.experimental.Deferred
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.launch
+
+class MainActivity : AppCompatActivity() {
+    private var job: Deferred<Unit>? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            if(editText.text != null) {
+                val url = editText.text.toString()
+                job = async {
+                    runTask(url)
+                }
+            }
+        }
+    }
+    private suspend fun runTask(inputURL:String) {
+        async(UI) {
+            textView.text = "start"
+        }.await()
+        launch {
+            textView.text = statusCheck(inputURL)
+        }.join()
+    }
+}
---
+++ app/src/main/java/net/teamfruit/androidyoutubedl/extURL.kt
@@ -0,0 +1,38 @@
+package net.teamfruit.androidyoutubedl
+
+import com.github.kittinunf.fuel.httpGet
+import kotlinx.coroutines.experimental.async
+import java.net.URLDecoder
+
+tailrec suspend fun getCodecsURL(inputURL:String):String {
+    val getVideoInfo = async {
+        val videoID = Regex("""v=[\d\p{Upper}\p{Lower}_\-]{11}""").find(inputURL)?.value?.substring(2..12)
+        val connect = "http://www.youtube.com/get_video_info?video_id=$videoID".httpGet().response().toString()
+        return@async URLDecoder.decode(URLDecoder.decode(connect)).replace("%2C",",").replace("%2F", "/").replace("+", " ")
+    }.await()
+    val codecsList:MutableList<String> = getVideoInfo.split(";").toMutableList()
+    var codecsUrl:String? = null
+    loop@for(list in codecsList) {
+        when {
+            Regex("""codecs=\"vorbis\"""").containsMatchIn(list) -> {
+                codecsUrl = list.substring(Regex("""url=h""").find(list)?.range?.last!!)
+                break@loop
+            }
+            Regex("""codecs=\"opus\"""").containsMatchIn(list) -> {
+                codecsUrl = list.substring(Regex("""url=h""").find(list)?.range?.last!!)
+                break@loop
+            }
+            else -> {}
+        }
+    }
+    return codecsUrl ?: getCodecsURL(inputURL)
+}
+tailrec suspend fun statusCheck(inputURL: String):String {
+    val codecsURL = getCodecsURL(inputURL)
+    val status = codecsURL.httpGet().response().second.statusCode
+    return when(status) {
+        200 -> { codecsURL }
+        403 -> { statusCheck(inputURL) }
+        else -> { statusCheck(inputURL) }
+    }
+}
