5bb4c1e19e2f216f4f41cb4d181a8d3bd197e986
KomeijiKaede <KomeijiKoishi200058@gmail.com>
2018-11-20 15:38:58 +0900

    忘れ物

--- app/src/main/java/net/teamfruit/androidyoutubedl/experimental/RecyclerViewFragment.kt
+++ app/src/main/java/net/teamfruit/androidyoutubedl/experimental/RecyclerViewFragment.kt
@@ -4,15 +4,25 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.support.v4.app.Fragment
 import android.content.Context
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.result.Result
+import com.google.gson.Gson
+import com.google.gson.JsonSyntaxException
 import kotlinx.android.synthetic.main.fragment_listview.*
 import net.teamfruit.androidyoutubedl.R
 import net.teamfruit.androidyoutubedl.experimental.database.Base
 import net.teamfruit.androidyoutubedl.experimental.database.DBEntity
+import net.teamfruit.androidyoutubedl.utils.MediaPlayerController
+import net.teamfruit.androidyoutubedl.utils.PlayerResponse
+import net.teamfruit.androidyoutubedl.utils.getBestQualityAudioFormatByAdaptiveFormats
+import net.teamfruit.androidyoutubedl.utils.getStringValueByUrlParameters
+import java.net.URLDecoder
 
 class RecyclerViewFragment: Fragment(), RecyclerViewAdapter.OnItemClickListener {
     private lateinit var appContext: Context
@@ -44,7 +54,42 @@ class RecyclerViewFragment: Fragment(), RecyclerViewAdapter.OnItemClickListener 
     }
 
     override fun onItemClick(entity: DBEntity) {
+        val gsonInstance = Gson()
+        "https://www.youtube.com/get_video_info?video_id=${entity.videoID}".httpGet().response { _, response, result ->
+            when(result) {
+                is Result.Success -> {
+                    val parsedJson : PlayerResponse
+                    try {
+                        parsedJson = gsonInstance.fromJson(
+                                URLDecoder.decode(
+                                        getStringValueByUrlParameters(
+                                                response.toString(),
+                                                "player_response"
+                                        ),
+                                        "UTF-8"
+                                ),
+                                PlayerResponse::class.java
+                        )
+                    } catch (e: JsonSyntaxException) {
+                        return@response
+                    }
 
+                    if (parsedJson.playabilityStatus.status == "UNPLAYABLE") return@response
+
+                    val format = getBestQualityAudioFormatByAdaptiveFormats(
+                            parsedJson.streamingData.adaptiveFormats
+                    )
+
+                    if(format === null) return@response
+
+                    MediaPlayerController.prepareStart(Uri.parse(format.url).toString())
+                }
+
+                is Result.Failure -> {
+                    return@response
+                }
+            }
+        }
     }
 
     override fun onItemLongClick(entity: DBEntity) {
